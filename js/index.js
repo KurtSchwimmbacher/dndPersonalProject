94fc7c352280d419867afc5926e000797d74baea
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-12-08 21:18:20 +0200

    dynamically pull races into character builder
    use api to pull races into character builder section and add list styling

--- js/index.js
+++ js/index.js
@@ -6,4 +6,48 @@ $(document).ready(function (){
         window.location.href = `characterSheet.html`;
     });
 
+    $("#makeCharacter").on('click',function(){
+        window.location.href = `characterBuilder.html`;
+    });
+
+    getClass();
+
 });
+
+
+function getClass(){
+    let classToDisp = "";
+    $.ajax({
+        dataType: 'json',
+        type:"GET",
+        // gets the first page of results
+        url:`https://www.dnd5eapi.co/api/races`,
+        
+        success: function(data){
+        temp = data.results;
+
+        loadClassesIntoCharacterBuilder(temp);    
+        
+        },
+        error: function(error){
+        // handle as it comes
+        }
+    })
+}
+
+function loadClassesIntoCharacterBuilder(racesArr){
+    console.log(racesArr)
+    $("#raceCon").empty();
+
+    racesArr.forEach(race => {
+        const listItem= $(`
+        <tr class="mt-5 mb-5">
+            <td scope="row" class="race-name">${race.name}</td>
+            <td class="read-more-race">Read More</td>
+        </tr>
+        `);
+    
+        $("#raceCon").append(listItem);
+    
+      });
+}
