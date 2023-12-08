58e3fd17636334cbb04f347397b0e124daa9f682
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-12-08 22:43:21 +0200

    basic modal implementation
    implement basic modal for race selection

--- js/index.js
+++ js/index.js
@@ -42,12 +42,45 @@ function loadClassesIntoCharacterBuilder(racesArr){
     racesArr.forEach(race => {
         const listItem= $(`
         <tr class="mt-5 mb-5">
-            <td scope="row" class="race-name">${race.name}</td>
-            <td class="read-more-race">Read More</td>
+            <td scope="row" class="race-name" >${race.name}</td>
+            <td class="read-more-race" data-bs-toggle="modal" data-bs-target="#exampleModal">Read More</td>
         </tr>
         `);
+
+        listItem.on('click','.read-more-race',function(){
+            fillRaceModal(race.name);
+        });
     
         $("#raceCon").append(listItem);
     
       });
+}
+
+
+function fillRaceModal(race){
+    let modalTitle = $("#raceInfo");
+    let modalContent = $("#modalBody");
+
+    race = race.toLowerCase();
+
+    $.ajax({
+        dataType: 'json',
+        type: "GET",
+        url: `https://www.dnd5eapi.co/api/races/${race}`,
+
+        success: function(data){
+            console.log(data)
+            modalTitle.text(data.name)
+            let abilityScoresLen = data.ability_bonuses.length;
+            let content = "";
+            for(i = 0; i < abilityScoresLen; i++){
+                content += data.ability_bonuses[i].ability_score.name + " +" + data.ability_bonuses[i].bonus + "\n"
+            }
+            modalContent.text(content);
+        },
+        error: function(){
+            // deal with
+        }
+    })
+
 }
